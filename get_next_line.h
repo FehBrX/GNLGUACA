0db0b833dd59d94087190b2f0ddc4f10681244f5
Fernando <ffernand@student.42sp.org.br>
2021-11-02 21:15:56 -0300

    UpdatenoH

--- get_next_line.h
+++ get_next_line.h
@@ -6,12 +6,10 @@
 /*   By: ffernand <ffernand@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/21 17:21:40 by ffernand          #+#    #+#             */
-/*   Updated: 2021/11/02 11:45:59 by ffernand         ###   ########.fr       */
+/*   Updated: 2021/11/02 21:12:22 by ffernand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
 # include <unistd.h>
 # include <stdio.h>
 # include <stdlib.h>
@@ -23,5 +21,3 @@ char	*ft_strdup(const char *s1);
 char	*ft_strjoin(char const *s1, char const *s2);
 size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 size_t	ft_strlen(const char *str);
-
-#endif
